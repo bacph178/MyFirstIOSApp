af76f50211db72105900a6119cbcd4bf1921bb18
Phung Hoang Bac <phung.hoang.bac@framgia.com>
2014-09-10 11:02:58 +0700

    Customize the Image

--- MyFirstIOSApp/Phung_Hoang_BacViewController.m
+++ MyFirstIOSApp/Phung_Hoang_BacViewController.m
@@ -17,29 +17,37 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-	// Do any additional setup after loading the view, typically from a nib.
-    self.mainSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(100, 100, 0, 0)];
-    
-    //add mainSwitch to view
+    self.mainSwitch = [[UISwitch alloc] initWithFrame:CGRectZero];
+    self.mainSwitch.center = self.view.center;
     [self.view addSubview:self.mainSwitch];
     
-    //khi switch chuyen trang thai thi ham switchIsChanged duoc goi
-    [self.mainSwitch addTarget:self action:@selector(switchIsChanged:) forControlEvents:UIControlEventValueChanged];
+    /* Customize the switch */
+    
+    /*
+     adjust the off-mode tint color
+     doi mau trang thai off cua switch
+    */
+    self.mainSwitch.tintColor = [UIColor redColor];
+    
+    /*
+     Adjust the on-mode tint color
+     doi mau trang thai on cua switch
+     */
+    self.mainSwitch.onTintColor = [UIColor blackColor];
+    
+    /*
+     Also change the knob's tint color
+     doi mau num di chuyen cua switch
+     */
+    self.mainSwitch.thumbTintColor = [UIColor greenColor];
+    
+    self.mainSwitch.onImage = [UIImage imageNamed:@"On"];
+    self.mainSwitch.offImage = [UIImage imageNamed:@"Off"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-//Ham xu ly khi chuyen trang thai cua mainSwitch
-- (void) switchIsChanged:(UISwitch *)paramSender {
-    if ([paramSender isOn]) {
-        NSLog(@"sang con me no rui");
-    } else {
-        NSLog(@"Lai tat rui ah");
-    }
 }
 @end
