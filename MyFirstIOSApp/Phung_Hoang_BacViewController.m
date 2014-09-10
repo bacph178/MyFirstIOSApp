f5a31473768a7909931d9573a09d18925d216de1
Phung Hoang Bac <phung.hoang.bac@framgia.com>
2014-09-10 14:58:45 +0700

    Picking Values with the UIPickerView

--- MyFirstIOSApp/Phung_Hoang_BacViewController.m
+++ MyFirstIOSApp/Phung_Hoang_BacViewController.m
@@ -8,8 +8,9 @@
 
 #import "Phung_Hoang_BacViewController.h"
 
-@interface Phung_Hoang_BacViewController ()
-@property (nonatomic, strong) UISwitch *mainSwitch;
+@interface Phung_Hoang_BacViewController () <UIPickerViewDataSource,
+UIPickerViewDelegate>
+@property (nonatomic, strong) UIPickerView *myPicker;
 @end
 
 @implementation Phung_Hoang_BacViewController
@@ -17,32 +18,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.mainSwitch = [[UISwitch alloc] initWithFrame:CGRectZero];
-    self.mainSwitch.center = self.view.center;
-    [self.view addSubview:self.mainSwitch];
+    self.myPicker = [[UIPickerView alloc] init];
     
-    /* Customize the switch */
+    /* them data vao picker */
+    self.myPicker.dataSource = self;
+    self.myPicker.center = self.view.center;
     
-    /*
-     adjust the off-mode tint color
-     doi mau trang thai off cua switch
-    */
-    self.mainSwitch.tintColor = [UIColor redColor];
-    
-    /*
-     Adjust the on-mode tint color
-     doi mau trang thai on cua switch
-     */
-    self.mainSwitch.onTintColor = [UIColor blackColor];
-    
-    /*
-     Also change the knob's tint color
-     doi mau num di chuyen cua switch
-     */
-    self.mainSwitch.thumbTintColor = [UIColor greenColor];
-    
-    self.mainSwitch.onImage = [UIImage imageNamed:@"On"];
-    self.mainSwitch.offImage = [UIImage imageNamed:@"Off"];
+    /* phat hien cham vao picker*/
+    self.myPicker.delegate = self;
+    [self.view addSubview:self.myPicker];
 }
 
 - (void)didReceiveMemoryWarning
@@ -50,4 +34,36 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/* tra lai so cot cua Picker*/
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    if ([pickerView isEqual:self.myPicker]) {
+        return 2;
+    }
+    return 0;
+}
+
+/* tra lai so thanh phan chon cua 1 cot chon cua Picker */
+- (NSInteger)pickerView:(UIPickerView *)pickerView
+numberOfRowsInComponent:(NSInteger)component {
+    if ([pickerView isEqual:self.myPicker]) {
+        return 5;
+    }
+    return 0;
+}
+
+/* Data hien thi tren cac hang cua moi cot */
+- (NSString *)pickerView:(UIPickerView *)pickerView
+             titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    if ([pickerView isEqual:self.myPicker]) {
+        return [NSString stringWithFormat:@"Row %ld", (long)row +1];
+    }
+    return nil;
+}
+
+/* neu muon thay doi gai tri cua picker trong qua trinh chay can phai reload lai
+ du lieu bang cach reload lai toan bo cac thanh phan bang ham reloadAllComponets
+ hoac reload lai 1 thanh phan bang ham reloadComponent voi bien truyen vao la chi
+ so cua component can reload
+ */
 @end
